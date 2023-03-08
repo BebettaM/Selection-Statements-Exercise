13f4fb2ccaecaf82589c0b8f0f24c51f7df8cb7a
Bebetta Motondo <claramotondo2@yahoo.fr>
2023-03-08 13:41:56 -0600

    selection statement

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -3,8 +3,74 @@
     public class Program
     {
         static void Main(string[] args)
+
+        // Exercise 1
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to the guessing game!");
+            Console.WriteLine("Can you guess my favorite number?");
+            Console.WriteLine("What is the upper Limit for range of the number?");
+
+            var userResponse = Console.ReadLine();
+
+            var upperLimit = int.Parse(userResponse);
+
+            var random = new Random();
+            var number = random.Next(2, upperLimit);
+
+            Console.WriteLine("Input your guess:");
+
+            var guess = int.Parse(Console.ReadLine());
+
+            if (guess > number)
+
+            {
+                Console.WriteLine("Sorry, your guess is too high!");
+            }
+
+            else if (guess < number)
+            {
+                Console.WriteLine("Your guess is too low! Sorry");
+            }
+            else
+            {
+                Console.WriteLine("You got it right!");
+            }
+
+    //Exercise 2
+
+    Console.WriteLine("What is your favorite TV show?");
+
+        var show = Console.ReadLine();
+
+       switch (show)
+            {
+        case "Save by the bell":
+            Console.WriteLine("Save by the bell is a great show");
+
+        break;
+
+        case "Friends":
+
+          Console.WriteLine("Friends, that is great!");
+
+        break;
+
+        case "Office":
+            Console.WriteLine("Office is a awsome show!");
+
+        break;
+
+        default:
+
+            Console.WriteLine("I have seen that show before.");
+         break;
+
+            }
+ 
+
         }
     }
 }
+
+       
+     
